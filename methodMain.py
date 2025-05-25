33b07c9d39e0773e5d63210e35bfdbe0660057bd
Sanschagrin <mah00027@algonquinlive.com>
2025-05-25 18:11:24 -0400

    Added main method used to run program and loop through records array to print. Added print name in main method.

--- methodMain.py
+++ methodMain.py
@@ -49,3 +49,16 @@ def CSVReader(csvData):
         
     #Return records array after being populated
     return records
+
+#Main method to run the program
+if __name__ == "__main__":
+
+    #Print my my name on to terminal so it is always visible
+    print("Gregory Mah 041114855")
+
+    #Set records array to hold value returned by CSVReader method
+    records = CSVReader(csvData)
+
+    #Loop through and print the list of objects
+    for record in records:
+        print(record)
