03aec8b1871821dbee36561cdbb45dc42afe4750
Sanschagrin <mah00027@algonquinlive.com>
2025-06-15 14:19:13 -0400

    Started creating view methods, changed refactored file IO to pull 100 records instead of 3

---
+++ view/recordView.py
@@ -0,0 +1,14 @@
+#Gregory Mah 041114855
+#CST 8002 020 Programming Language Research Proj
+#Stanley Pieda
+#Due 2025-06-14
+
+def welcome(me = "Gregory Mah 41114855"):
+    print(f"Welcome to the menu")
+    print(f"By {me}\n")
+
+def menu():
+    print("Menu")
+    print("1. Reload file")
+    print("2. Save data to new file")
+    print("")
