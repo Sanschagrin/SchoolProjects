dc1658f25972d4bcc31a39f3db7ad78bfd370487
Sanschagrin <mah00027@algonquinlive.com>
2025-06-15 16:28:49 -0400

    Finished view methods to connect to controller class

--- view/recordView.py
+++ view/recordView.py
@@ -10,5 +10,42 @@ def welcome(me = "Gregory Mah 41114855"):
 def menu():
     print("Menu")
     print("1. Reload file")
-    print("2. Save data to new file")
-    print("")
+    print("2. Select single record")
+    print("3. Select multiple records")
+    print("4. Create new record ")
+    print("5. Edit a record")
+    print("6. Delete a record")
+    print("7. Save data to new file")
+
+def userID():
+    return input("Enter NPRIID of your selected record")
+
+def allRecords(records):
+    if not records:
+        print("File is empty")
+        return
+    for record in records:
+        print(record)
+
+def newRecord():
+    print("Enter record details below")
+    return {
+        "NPRID": input("NPRIID: "), "facility": input("Facility: "), "company": input("Company: "), 
+        "address": input("Address: "), "city": input("City: "), "province": input("Province: "), 
+        "postal": input("Postal Code: "), "lat": input("Latitude: "), "long": input("Longitude: "), 
+        "emissions": input("Emission Type: "), "units": input("Emission Units: "), "details": input("Details: "),
+        "info": input("Information: "), "year": input("Year")
+
+    }
+
+def displayRecord(record):
+    print("Record Details")
+    print (record)
+
+def message(msg):
+    print(f"Information: {msg}"
+    )
+
+def error(msg):
+    print(f"ERROR: {msg}"
+          )
